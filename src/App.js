3f602096c19fa5393404ec953a61721f39818b95
Siddharth verma <74046564+sidharthviz@users.noreply.github.com>
2023-01-11 18:07:16 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -4,23 +4,106 @@ import "./styles.css";
 
 const Mybooks = {
   Blockchain: [
-    { name: "Mastering Ethereum", Rating: "4.8/5" },
-    { name: "Solana in Nutshell", Rating: "4.7/5" }
+    {
+      name: "Mastering Ethereum",
+      Rating: " Rating: ( 4.5 / 5 )",
+      Description: "A perfect Book for Understand the core of Ethereum."
+    },
+    {
+      name: "Solana in nutshell",
+      Rating: " Rating: (5 / 5)",
+      Description:
+        "Understand the Core use of Solana and why it's much faster in development."
+    },
+    {
+      name: "Mastering Bitcoin",
+      Rating: "Rating: (4 / 5)",
+      Description:
+        "Go Through the White Paper in order to Understand the use of Blockchain."
+    },
+    {
+      name: "Ethereum Yellow paper",
+      Rating: "Rating: (5 / 5)",
+      Description:
+        "How Ethereum 2.0 is Changing the whole Blockchain World in Proof of stake."
+    }
+  ],
+
+  Psychology: [
+    {
+      name: "Thinking Fast & Slow",
+      Rating: " Rating: ( 5 / 5 )",
+      Description:
+        " Why we do such things and how the brains are wired by two process."
+    },
+    {
+      name: "Games People Play",
+      Rating: " Rating: ( 4.8 / 5 )",
+      Description: "A deep Psychology of Human Relationship."
+    },
+    {
+      name: "Deviate",
+      Rating: " Rating: ( 4.5 / 5 )",
+      Description: " Science of seeing things diffrently."
+    },
+    {
+      name: "Think Again",
+      Rating: " Rating: ( 4 / 5 )",
+      Description: "Everything we think deserves a second Chance."
+    }
   ],
 
   NonFiction: [
-    { name: "Almanack of Naval Ravikant", Rating: "5/5" },
-    { name: "The Selfish Gene", Rating: "4.5/5" }
+    {
+      name: "Homo-Dues",
+      Rating: " Rating: ( 4.3 / 5 )",
+      Description: "The Brief History Of Tomorrow."
+    },
+    {
+      name: "Selfish Gene",
+      Rating: " Rating: ( 4.6 / 5 )",
+      Description:
+        "The Theory of Evolution and How it's all Connected to our Genes."
+    },
+    {
+      name: "4 Hour Work Week",
+      Rating: " Rating: ( 4.7 / 5 )",
+      Description:
+        "Escape 9-5 and Live Anywhere Around the World, Join the New Rich."
+    },
+    {
+      name: "Life 3.0",
+      Rating: " Rating: ( 4.5 / 5 )",
+      Description: "Being Human in the age of AI."
+    }
   ],
-
   Business: [
-    { name: "Rework", Rating: "4.5/5" },
-    { name: "Zero to 1", Rating: "4.8/5" }
+    {
+      name: "Rework",
+      Rating: " Rating: ( 4 / 5 )",
+      Description: " Art of starting again."
+    },
+    {
+      name: "Zero To 1",
+      Rating: " Rating: ( 4.2 / 5 )",
+      Description: " How to Start a Business From Scratch."
+    },
+    {
+      name: "$100M Offer ",
+      Rating: " Rating: ( 4.8 / 5 )",
+      Description: " How to make an offer Nobody can refuse."
+    },
+    {
+      name: "No Rules Rules",
+      Rating: " Rating: ( 5 / 5 )",
+      Description: "Culture of Reinvention."
+    }
   ]
 };
 
 export default function App() {
   const [getBooks, setBooks] = useState("Blockchain");
+
   function BookHandler(fiction) {
     setBooks(fiction);
   }
@@ -39,12 +122,13 @@ export default function App() {
         ))}
       </div>
       <hr />
-          {Mybooks[getBooks].map((author) => (
-            <li className="list">
-              <div style={{ fontSize: "larger" }}> {author.name} </div>
-              <div style={{ fontSize: "smaller" }}> {author.Rating} </div>
-            </li>
-          ))}
-      </div>
+      {Mybooks[getBooks].map((author) => (
+        <li className="list">
+          <div style={{ fontSize: "Larger" }}> {author.name} </div>
+          <div style={{ fontSize: "smaller" }}> {author.Rating} </div>
+          <div style={{ fontSize: "medium" }}> {author.Description} </div>
+        </li>
+      ))}
+    </div>
   );
 }
